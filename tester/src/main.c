36782d07d334c603e72607604b9035389798f3b4
Sqyd <wojkap10@proton.me>
2025-11-16 21:04:55 +0100

    Tester

--- tester/src/main.c
+++ tester/src/main.c
@@ -1,2 +1,28 @@
 #include "../../include/esclib.h"
 
+#include <stdio.h>
+
+
+void InitTest() {
+	InitTui(60, false);
+
+	while(1) {
+		
+	}
+
+	CloseTui();
+}
+
+int main(void) {
+	int Input = 0;
+
+	printf("1. Backbuffor WriteTo test\n");
+
+	printf("Input: ");
+	scanf("%d", &Input);
+
+	if(Input == 1) { InitTest(); }
+	else if(Input == 2) { return 0; }
+	
+	return 0;
+}
